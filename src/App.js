bb6021eaa6dff079129d001a134f2064b9975561
ptthao0801 <ptthao0801@gmail.com>
2024-07-02 17:38:03 +0700

    first cm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Hello from './Hello';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: true
+        };
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    delete = () => {
+        this.setState({ display: false });
+    };
+
+    render() {
+        let comp;
+        if (this.state.display) {
+            comp = <Hello />;
+        }
+        return (
+            <div style={{ textAlign: 'center' }}>
+                {comp}
+                <button onClick={this.delete}>
+                    Delete the component
+                </button>
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+class Hello extends Component {
+    componentWillUnmount() {
+        alert('The component is going to be unmounted');
+    }
+
+    render() {
+        return <h1>Hello World</h1>
+    }
+}
+export default Hello;
